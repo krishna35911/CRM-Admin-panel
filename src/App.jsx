c6d098a8d1cbb5707dca58f29eeb372f0797736a
krishna35911 <krishnakp35911@gmail.com>
2024-04-29 10:54:05 +0530

    login design completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Login from './components/Login'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
